ffc27eafde8119b931303a2324e33a22d50f6eb1
stella-celentano <stellacelentano99@gmail.com>
2020-10-02 11:16:57 -0300

    Implementando mensagem de API conectada

--- src/index.js
+++ src/index.js
@@ -4,12 +4,20 @@ const routes = require('./routes');
 
 const app = express();
 
+const PORT = 33333
+
 app.use(cors({
     // origin: 'http://meuapp.com' // quando colocar em produção
 }));
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.get('/', (req, res) => res.send({ message: `API listening in port ${PORT}` }) )
+
+app.use('*', (req, res) => res.send({ message: 'API not found' }))
+
+app.listen(PORT, () => {
+    console.log(`API listening in port ${PORT}`)
+});
 
 module.exports = app;
